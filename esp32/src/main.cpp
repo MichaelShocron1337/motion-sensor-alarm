59748c4562aaf92c6fdd5e2ddcfb2753dd825f02
Michael Shocron <michael.shocron@hotmail.se>
2026-09-23 18:12:31 +0200

    Update main.cpp

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,1 +1,32 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
+#include <sensorConfig.h>
+#include <httpConfig.h>
+#include <connectionConfig.h>
+#include <WiFiClientSecure.h>
+
+
+void setup()
+{
+  activatePin();
+  ConnectionControl();
+  Serial.begin(115200);
+}
+void loop()
+{
+  motionDetected = false;
+  if(motionDetected)
+  {
+    String currentMeasurement = createMeasurement();
+    if(!currentMeasurement.isEmpty())
+    {
+      sendMeasurement(currentMeasurement);
+    }
+    Serial.println("Motion Detected!");
+  }
+
+}
+
 
